c71f08e5dc2ce63a689523e0bede8cb1d2689ea1
Alexander <valverin95@gmail.com>
2022-12-02 08:36:16 +0300

    fix: floor diff to up

--- src/app/shared/components/image-card/image-card.component.ts
+++ src/app/shared/components/image-card/image-card.component.ts
@@ -46,7 +46,7 @@ export class ImageCardComponent implements OnChanges {
     console.log(changes);
     if (this.compressedImage && this.image) {
       const percent = 100 - ((100 / this.image.file.size) * this.compressedImage.size);
-      this.diff = Math.floor(percent);
+      this.diff = Math.ceil(percent);
       this.ref.detectChanges()
     }
   }
