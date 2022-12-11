f3061e46ae54f2097ff1dd9f384097e7f7a54184
Alexander <valverin95@gmail.com>
2022-12-11 22:08:39 +0300

    feat: add converter to mb

--- ssr/src/services/converter.ts
+++ ssr/src/services/converter.ts
@@ -2,4 +2,8 @@ function bytesToKb(value: number): string {
   return (value / 1000).toFixed(1);
 }
 
-export const convertService = Object.assign({}, {bytesToKb})
+function bytesToMb(value: number) {
+  return (value / 1000_000).toFixed(2);
+}
+
+export const convertService = Object.assign({}, {bytesToKb, bytesToMb});
