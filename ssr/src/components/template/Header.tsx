f6b5b868798c65d450a2f6e4ce211e63a88ad924
Alexander <valverin95@gmail.com>
2022-12-13 00:12:02 +0300

    feat: accent header

--- ssr/src/components/template/Header.tsx
+++ ssr/src/components/template/Header.tsx
@@ -1,6 +1,6 @@
 export const Header = () => {
   return (
-    <div class="navbar bg-base-200">
+    <div class="navbar bg-neutral text-neutral-content">
       <a class="btn btn-ghost normal-case text-xl" href="/">Compressor</a>
     </div>
   )
