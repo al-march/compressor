73bf331bf96d2c46bee7ace88a0394ed1371bc2f
Alexander <valverin95@gmail.com>
2022-12-13 21:01:54 +0300

    build v0.0.17 with sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,15 +2,13 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 import solidJs from "@astrojs/solid-js";
-
-// https://astro.build/config
 import tailwind from "@astrojs/tailwind";
+import sitemap from '@astrojs/sitemap';
 
-// https://astro.build/config
 export default defineConfig({
   site: 'https://compress.monster',
   base: './',
   output: 'static',
   outDir: './dist',
-  integrations: [solidJs(), tailwind()]
+  integrations: [solidJs(), tailwind(), sitemap()]
 });
