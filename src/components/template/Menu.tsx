7b3495d45a058bfc514f83556b00e20e77937892
Alexander <valverin95@gmail.com>
2022-12-13 20:53:38 +0300

    feat: add new pages

---
+++ src/components/template/Menu.tsx
@@ -0,0 +1,11 @@
+export const Menu = () => {
+  return (
+    <>
+      <a class="link link-hover" href="/">Главная</a>
+      <a class="link link-hover" href="/about">О продукте</a>
+      <a class="link link-hover" href="/features">Возможности</a>
+      <a class="link link-hover" href="/contact">Контакты</a>
+      <a class="link link-hover" href="/faq">FAQ</a>
+    </>
+  )
+}
