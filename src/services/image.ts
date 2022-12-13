f480fdddce840f4c9af94af9aaedb1f9b2798e92
Alexander <valverin95@gmail.com>
2022-12-13 20:09:45 +0300

    fix: imageService

--- src/services/image.ts
+++ src/services/image.ts
@@ -1,12 +1,12 @@
-import { Image } from "../models/image.model";
+import { CompressImage } from "@app/models";
 
 function fileListToImages(list: FileList) {
-  const images: Image[] = [];
+  const images: CompressImage[] = [];
 
   for (let i = 0; i < list.length; i++) {
     const file = list[i];
     if (file instanceof File) {
-      const image = new Image(file);
+      const image = new CompressImage(file);
       images.push(image);
     }
   }
