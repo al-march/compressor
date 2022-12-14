c4953100a9ef41e7073c88c6cbb613ada5ce8a87
Alexander <valverin95@gmail.com>
2022-12-14 19:40:47 +0300

    feat: add Card components

---
+++ src/components/template/layout/Cards.tsx
@@ -0,0 +1,53 @@
+import type { DaisyColor } from "@app/constants";
+import type { ParentProps } from "solid-js";
+import { H4 } from "../typography";
+
+type CardContainerProps = {
+  class?: string;
+}
+
+export const CardContainer = (props: ParentProps<CardContainerProps>) => {
+  return (
+    <div
+      class="flex items-start justify-center gap-4 flex-wrap"
+      classList={{ [props.class || '']: !!props.class }}
+    >
+      {props.children}
+    </div>
+  )
+}
+
+type CardProps = {
+  icon?: string;
+  title?: string;
+  description?: string;
+  color?: DaisyColor;
+}
+
+export const Card = (props: ParentProps<CardProps>) => {
+  return (
+    <article
+      class="w-48 flex flex-col gap-2 items-center justify-center text-center"
+    >
+      <span
+        class="material-symbols-outlined text-4xl"
+        classList={{
+          'text-primary': props.color === 'primary',
+          'text-secondary': props.color === 'secondary',
+          'text-accent': props.color === 'accent',
+          'text-info': props.color === 'info',
+          'text-success': props.color === 'success',
+          'text-warning': props.color === 'warning',
+          'text-error': props.color === 'error',
+        }}
+      >
+        {props.icon}
+      </span>
+
+      <H4>{props.title}</H4>
+      <p>{props.description}</p>
+
+      {props.children}
+    </article>
+  )
+}
