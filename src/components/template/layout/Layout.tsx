2e1ad0c0eff5a73029f766f3e15c2b1979b8f312
Alexander <valverin95@gmail.com>
2022-12-14 18:50:43 +0300

    feat: update about page

--- src/components/template/layout/Layout.tsx
+++ src/components/template/layout/Layout.tsx
@@ -1,12 +1,21 @@
 import type { ParentProps } from "solid-js";
 
+type ContainerSize = 'sm';
+
 type Props = {
   class?: string;
+  size?: ContainerSize;
 }
 
 export const Container = (props: ParentProps<Props>) => {
   return (
-    <div class="container p-2 md:p-4" classList={{ [props.class || '']: !!props.class }}>
+    <div
+      class="container p-2 md:p-4"
+      classList={{
+        [props.class || '']: !!props.class,
+        'max-w-2xl': props.size === 'sm',
+      }}
+    >
       {props.children}
     </div>
   )
