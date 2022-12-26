2c487a5314335c25086f402d1212f00d1589f137
Alexander <valverin95@gmail.com>
2022-12-26 11:17:15 +0300

    feat: add loader for example

--- src/components/base/Loader.tsx
+++ src/components/base/Loader.tsx
@@ -4,21 +4,27 @@ export type LoaderSize = typeof LoaderSizes[number];
 type Props = {
   class?: string;
   size?: LoaderSize;
+  center?: boolean;
 }
 
 export const Loader = (props: Props) => {
   return (
-    <div
-      class="lds-ring w-8 h-8"
-      classList={{
-        [props.class || '']: !!props.class,
-        'w-2 h-2': props.size === 'xs',
-        'w-4 h-4': props.size === 'sm',
-        'w-8 h-8': props.size === 'md',
-        'w-12 h-12': props.size === 'lg',
-      }}
-    >
-      <div></div><div></div><div></div><div></div>
+    <div classList={{
+      'flex justify-center': props.center
+    }}>
+      <div
+        class="lds-ring w-8 h-8"
+        classList={{
+          [props.class || '']: !!props.class,
+          'w-2 h-2': props.size === 'xs',
+          'w-4 h-4': props.size === 'sm',
+          'w-8 h-8': props.size === 'md',
+          'w-12 h-12': props.size === 'lg',
+        }}
+      >
+        <div></div><div></div><div></div><div></div>
+      </div>
     </div>
+
   )
 }
--- src/pages/index/CompressExample.tsx
+++ src/pages/index/CompressExample.tsx
@@ -1,11 +1,13 @@
-import { ImageCompareSlider } from "@app/components/base"
+import { ImageCompareSlider, Loader } from "@app/components/base"
+import { Scale } from "@app/components/base/animations";
 import { ResultStats } from "@app/components/compress/result";
 import { H3 } from "@app/components/template/typography";
 import { CompressImage } from "@app/models";
 import { compressorService } from "@app/services";
-import { createSignal, onMount } from "solid-js"
+import { createSignal, onMount, Show } from "solid-js"
 
 export const CompressExample = () => {
+  const [load, setLoad] = createSignal(true);
   const [img, setImg] = createSignal<File>();
   const [images, setImages] = createSignal<CompressImage[]>([]);
   const [beforeSrc, setBeforeSrc] = createSignal('');
@@ -32,22 +34,27 @@ export const CompressExample = () => {
       setBeforeSrc(beforeSrc);
       setAfterSrc(afterSrc);
       setImages([before]);
+      setLoad(false);
     }
   })
 
   return (
-    <div class="flex flex-col gap-8">
-      <ImageCompareSlider
-        class="rounded overflow-hidden"
-        before={<img src={beforeSrc()} alt="before image" />}
-        after={<img src={afterSrc()} alt="before image" />}
-      />
+    <Scale>
+      <Show when={!load()} fallback={<Loader center />}>
+        <div class="flex flex-col gap-8">
+          <ImageCompareSlider
+            class="rounded overflow-hidden"
+            before={<img src={beforeSrc()} alt="before image" />}
+            after={<img src={afterSrc()} alt="before image" />}
+          />
 
-      <div class="flex justify-evenly items-center gap-4">
-        <H3 class="text-error">До</H3>
-        <ResultStats images={images()} />
-        <H3 class="text-success">После</H3>
-      </div>
-    </div>
+          <div class="flex justify-evenly items-center gap-4">
+            <H3 class="text-error">До</H3>
+            <ResultStats images={images()} />
+            <H3 class="text-success">После</H3>
+          </div>
+        </div>
+      </Show>
+    </Scale>
   )
 }
