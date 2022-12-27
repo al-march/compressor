607e2390dc56ebe43a8769392e795e5438d3d95c
Alexander <valverin95@gmail.com>
2022-12-27 17:47:32 +0300

    feat: [Link] auto active class

--- src/components/base/link/Link.tsx
+++ src/components/base/link/Link.tsx
@@ -6,19 +6,31 @@ type Props = {
 } & JSX.DOMAttributes<HTMLAnchorElement>;
 
 export const Link = (props: ParentProps<Props>) => {
+  let ref: HTMLAnchorElement;
+
   const [local, others] = splitProps(props, ['to', 'class', 'classList']);
   const [isActive, setIsActive] = createSignal(false);
+  const [activeClass, setActiveClass] = createSignal('');
 
   onMount(() => {
     const isActive = location.pathname === local.to;
     setIsActive(isActive);
+
+    if (ref) {
+      if (ref.classList.contains('btn')) {
+        setActiveClass('btn-active');
+      } else {
+        setActiveClass('link-active');
+      }
+    }
   })
 
   return (
     <a
+      ref={el => ref = el}
       href={local.to}
       classList={{
-        'btn-active': !!isActive(),
+        [activeClass()]: !!isActive(),
         [local.class || '']: !!local.class,
         ...local.classList
       }}
