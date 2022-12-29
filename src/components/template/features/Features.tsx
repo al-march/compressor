ce776c6cfd38078bad2422bd9cd025ff2c742f19
Alexander <valverin95@gmail.com>
2022-12-29 16:08:34 +0300

    fix: remove Product scheme from feature

--- src/components/template/features/Features.tsx
+++ src/components/template/features/Features.tsx
@@ -3,18 +3,16 @@ import { FeatureCard } from './components';
 
 export const Features = () => {
   return (
-    <div
-      itemscope itemtype="http://schema.org/Product"
-    >
+    <div>
       <FeatureCard
         class="mb-8"
-        icon={<img itemprop="image" src="speed.svg" alt="" />}
+        icon={<img src="speed.svg" alt="" />}
       >
-        <H2 itemprop="name" class="mb-4">
+        <H2 class="mb-4">
           <strong class="font-black">Легче</strong> изображения - <br /> выше <strong class="font-black">скорость</strong>!
         </H2>
 
-        <p itemprop="description" class="font-light leading-6">
+        <p class="font-light leading-6">
           Изображения в интернете часто слишком большие для использования.
           Размер картинок напрямую влияет на рейтинг сайта и комфорт пользователей.
         </p>
@@ -23,26 +21,26 @@ export const Features = () => {
       <FeatureCard
         class="mb-8"
         reverse
-        icon={<img itemprop="image" src="rating.svg" alt="" />}
+        icon={<img src="rating.svg" alt="" />}
       >
-        <H2 itemprop="name" class="mb-4">
+        <H2 class="mb-4">
           Оптимизируйте <strong class="font-black">Lighthouse</strong> и <strong class="font-black">SEO</strong>
         </H2>
 
-        <p itemprop="description" class="font-light leading-6">
+        <p class="font-light leading-6">
           Сжатие картинок без ущерба для сайта скинуть несколько мегабайт без потерь в качестве <br />
           Получите бесплатный прирост балов в Lighthouse, подняв свой рейтинг в поисковой выдаче.
         </p>
       </FeatureCard>
 
       <FeatureCard
-        icon={<img itemprop="image" src="quality.svg" alt="" />}
+        icon={<img src="quality.svg" alt="" />}
       >
-        <H2 itemprop="name" class="mb-4">
+        <H2 class="mb-4">
           <strong class="font-black">Качественное</strong> и <strong class="font-black">неограниченное</strong> сжатие изображений
         </H2>
 
-        <p itemprop="description" class="font-light leading-6">
+        <p class="font-light leading-6">
           Здесь вы можете сжать неограниченное кол-во изображений любого размера.
           Это безопасно и быстро - ваши картинки никуда не загружаются, обрабатываются
           исключительно внутри браузера.
